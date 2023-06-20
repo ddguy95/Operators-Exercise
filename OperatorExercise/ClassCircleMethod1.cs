ad318d60f66f1083a4c45ea19288752a21b27872
derrickdrye <dryd95@hotmail.com>
2023-06-20 15:19:50 -0500

    completion of operators exercise

---
+++ OperatorExercise/ClassCircleMethod1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class ClassCircleMethod1
+    {
+        public static void AreaOfCircle(double radius)
+        {
+            double area;
+
+            area = (Math.PI * radius * radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+        }
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is quotient {quotient} remainder {remainder}");
+            }
+
+
+            //Exercise 2 see ClassCircleMethod1
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            ClassCircleMethod1.AreaOfCircle(radius);
+
         }
     }
 }
